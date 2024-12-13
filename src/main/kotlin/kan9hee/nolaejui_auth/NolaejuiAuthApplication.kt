6059bcb2a191a237c192cf35a37b4005accb1275
han <hcsy1219@gmail.com>
2024-12-13 09:59:48 +0900

    Init: Upload auth microservice project

---
+++ src/main/kotlin/kan9hee/nolaejui_auth/NolaejuiAuthApplication.kt
@@ -0,0 +1,11 @@
+package kan9hee.nolaejui_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NolaejuiAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<NolaejuiAuthApplication>(*args)
+}
---
+++ src/test/kotlin/kan9hee/nolaejui_auth/NolaejuiAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package kan9hee.nolaejui_auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NolaejuiAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

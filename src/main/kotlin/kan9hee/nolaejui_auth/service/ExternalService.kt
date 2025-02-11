082d31bd0446d2956c6c679f3c892fe09ef6566a
han <hcsy1219@gmail.com>
2025-02-11 15:01:15 +0900

    refactor: Split playlist create and delete grpc function

--- src/main/kotlin/kan9hee/nolaejui_auth/service/ExternalService.kt
+++ src/main/kotlin/kan9hee/nolaejui_auth/service/ExternalService.kt
@@ -8,13 +8,22 @@ import net.devh.boot.grpc.client.inject.GrpcClient
 class ExternalService(@GrpcClient("nolaejui-playlist")
                       private val playlistStub:MusicListServerGrpcKt.MusicListServerCoroutineStub) {
 
-    suspend fun createAndDeletePlaylistForUser(userName:String,isCreate:Boolean){
-        val request = Auth.UserCD.newBuilder()
+    suspend fun createDefaultPickupPlaylist(userName:String){
+        val request = Auth.UserName.newBuilder()
             .setUserName(userName)
-            .setIsCreate(isCreate)
             .build()
 
-        val response = playlistStub.createAndDeletePlaylistForUser(request)
+        val response = playlistStub.createDefaultPickupPlaylist(request)
+        if(!response.isSuccess)
+            throw RuntimeException(response.resultMessage)
+    }
+
+    suspend fun deleteUsersAllPlaylist(userName:String){
+        val request = Auth.UserName.newBuilder()
+            .setUserName(userName)
+            .build()
+
+        val response = playlistStub.deleteUsersAllPlaylist(request)
         if(!response.isSuccess)
             throw RuntimeException(response.resultMessage)
     }
